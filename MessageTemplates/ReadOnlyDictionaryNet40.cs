0abc8330b631a53ed9852f96152a98c9608026fc
Adam Chester <adamchester@gmail.com>
2015-07-01 19:16:28 +1000

    Reformat ReadOnlyDictionaryNet40

--- MessageTemplates/ReadOnlyDictionaryNet40.cs
+++ MessageTemplates/ReadOnlyDictionaryNet40.cs
@@ -3,23 +3,8 @@
 namespace System.Collections.Generic
 {
 #if USE_READONLY_COLLECTION_40
-    //
-    // Summary:
-    //     Represents a strongly-typed, read-only collection of elements.
-    //
-    // Type parameters:
-    //   T:
-    //     The type of the elements.This type parameter is covariant. That is, you can use
-    //     either the type you specified or any type that is more derived. For more information
-    //     about covariance and contravariance, see Covariance and Contravariance in Generics.
     public interface IReadOnlyCollection<out T> : IEnumerable<T>, IEnumerable
     {
-        //
-        // Summary:
-        //     Gets the number of elements in the collection.
-        //
-        // Returns:
-        //     The number of elements in the collection.
         int Count { get; }
     }
 
@@ -28,77 +13,10 @@ namespace System.Collections.Generic
         IEnumerable<KeyValuePair<TKey, TValue>>,
         IEnumerable
     {
-        //
-        // Summary:
-        //     Gets the element that has the specified key in the read-only dictionary.
-        //
-        // Parameters:
-        //   key:
-        //     The key to locate.
-        //
-        // Returns:
-        //     The element that has the specified key in the read-only dictionary.
-        //
-        // Exceptions:
-        //   T:System.ArgumentNullException:
-        //     key is null.
-        //
-        //   T:System.Collections.Generic.KeyNotFoundException:
-        //     The property is retrieved and key is not found.
         TValue this[TKey key] { get; }
-
-        //
-        // Summary:
-        //     Gets an enumerable collection that contains the keys in the read-only dictionary.
-        //
-        // Returns:
-        //     An enumerable collection that contains the keys in the read-only dictionary.
         IEnumerable<TKey> Keys { get; }
-        //
-        // Summary:
-        //     Gets an enumerable collection that contains the values in the read-only dictionary.
-        //
-        // Returns:
-        //     An enumerable collection that contains the values in the read-only dictionary.
         IEnumerable<TValue> Values { get; }
-
-        //
-        // Summary:
-        //     Determines whether the read-only dictionary contains an element that has the
-        //     specified key.
-        //
-        // Parameters:
-        //   key:
-        //     The key to locate.
-        //
-        // Returns:
-        //     true if the read-only dictionary contains an element that has the specified key;
-        //     otherwise, false.
-        //
-        // Exceptions:
-        //   T:System.ArgumentNullException:
-        //     key is null.
         bool ContainsKey(TKey key);
-        //
-        // Summary:
-        //     Gets the value that is associated with the specified key.
-        //
-        // Parameters:
-        //   key:
-        //     The key to locate.
-        //
-        //   value:
-        //     When this method returns, the value associated with the specified key, if the
-        //     key is found; otherwise, the default value for the type of the value parameter.
-        //     This parameter is passed uninitialized.
-        //
-        // Returns:
-        //     true if the object that implements the System.Collections.Generic.IReadOnlyDictionary`2
-        //     interface contains an element that has the specified key; otherwise, false.
-        //
-        // Exceptions:
-        //   T:System.ArgumentNullException:
-        //     key is null.
         bool TryGetValue(TKey key, out TValue value);
     }
 
@@ -123,13 +41,16 @@ namespace System.Collections.Generic
         public IEnumerable<TKey> Keys { get { return this.dictionary.Keys; } }
         public IEnumerable<TValue> Values { get { return this.dictionary.Values; } }
         public bool ContainsKey(TKey key) { return this.dictionary.ContainsKey(key); }
-        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() {
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
             return this.dictionary.GetEnumerator();
         }
-        public bool TryGetValue(TKey key, out TValue value) {
+        public bool TryGetValue(TKey key, out TValue value)
+        {
             return this.dictionary.TryGetValue(key, out value);
         }
-        IEnumerator IEnumerable.GetEnumerator() {
+        IEnumerator IEnumerable.GetEnumerator()
+        {
             return ((IEnumerable)this.dictionary).GetEnumerator();
         }
     }
@@ -144,15 +65,21 @@ namespace System.Collections.Generic
         }
         public T this[int index] { get { return this.elements[index]; } }
         public int Count { get { return this.elements.Length; } }
-        public IEnumerator<T> GetEnumerator() {
+        public IEnumerator<T> GetEnumerator()
+        {
             return this.elements.AsEnumerable().GetEnumerator();
         }
-        IEnumerator IEnumerable.GetEnumerator() {
-            return ((IEnumerable)this.elements).GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.elements.GetEnumerator();
         }
     }
 #endif
 
+    /// <summary>
+    /// Allows support both .NET 4.0 (which does not have IReadOnly* collection
+    /// interfaces) and .NET 4.5+ (which does have them).
+    /// </summary>
     public static class Net40ReadOnlyDictionaryExtensions
     {
         public static IReadOnlyList<T> ToListNet40<T>(
