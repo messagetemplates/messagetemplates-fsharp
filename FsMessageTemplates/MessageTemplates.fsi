7aa4298173b1c3ed85a040d37c5e82a929160e54
Adam Chester <adamchester@gmail.com>
2015-07-12 14:27:40 +1000

    Created an F#/Regex implemention

---
+++ FsMessageTemplates/MessageTemplates.fsi
@@ -0,0 +1,43 @@
+﻿module FsMessageTemplates.MessageTemplates
+
+/// Describes the destructuring hint, parsed from within a <see cref="PropertyToken" />.
+type Destructure = Default | Destructure | Stringify
+
+/// Represents a property parsed from within a message template.
+type PropertyTokenInfo = {
+    /// The property name extracted from the template message.
+    Name: string
+    /// The destructuring hint.
+    DestructuringHint: Destructure option
+    /// The alignment string (e.g. ",-10")
+    Align: string option
+    /// The 'format' string (e.g. ":0000")
+    Format: string option
+    /// The index into the string where the first character of the token was found
+    Index: int }
+
+/// The index into the string where the first character of the token was found
+type TextTokenInfo = {
+    /// The literal text value.
+    Text: string
+    /// The index into the string where the first character of the token was found
+    Index: int }
+
+type Token =
+| TextToken of TextTokenInfo
+| PropertyToken of PropertyTokenInfo
+
+/// A template, including the message and parsed properties.
+type Template = {
+    FormatString: string
+    Tokens: Token list }
+
+/// Parses a message template string.
+val parse: templateString:string -> Template
+
+/// Formats a message template as a string, replacing the properties
+/// with the provided values.
+val format: provider: System.IFormatProvider
+            -> t:Template
+            -> [<System.ParamArray>]values: obj[]
+            -> string
---
+++ FsMessageTemplates/Script.fsx
@@ -0,0 +1,97 @@
+﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#I "bin/Debug/"
+#r "FsMessageTemplates"
+#I "../packages/xunit.assert.2.0.0/lib/portable-net45+win+wpa81+wp80+monotouch+monoandroid+Xamarin.iOS/"
+#r "xunit.assert"
+
+open FsMessageTemplates.MessageTemplates
+
+type TestItem = { Template: string
+                  Values: System.Collections.IEnumerable
+                  Expected: Token list
+                  ExpectedString: string }
+
+let emptyProp = { Name=""; Index=0; Align=None; DestructuringHint=None; Format=None; }
+
+/// Named property token with an index
+let np name index =
+    PropertyToken({ emptyProp with
+                        Name = name
+                        Index = index })
+
+/// Destructured Named property token with an index
+let dnp name index =
+    PropertyToken({ emptyProp with
+                        Name = name
+                        DestructuringHint = Some Destructure.Destructure
+                        Index = index })
+
+/// Stringify Named property token with an index
+let snp name index =
+    PropertyToken({ emptyProp with
+                        Name = name
+                        DestructuringHint = Some Destructure.Stringify
+                        Index = index })
+
+/// Positional property token with an index
+let pp (position:int) index =
+    PropertyToken({ emptyProp with
+                        Name = sprintf "%i" position
+                        Index = index })
+
+/// Text token with an index
+let tx text index = TextToken({ Text = text; Index = index })
+
+let testData = [
+    { Template =        "test {test}";          Values = [123]
+      ExpectedString =  "test 123"
+      Expected =        [ tx "test " 0
+                          np "test" 5 ] }
+
+    { Template =        "test {test}{test}.";   Values = [123;456]
+      ExpectedString =  "test 123456."
+      Expected =        [ tx "test " 0
+                          np "test" 5
+                          np "test" 11
+                          tx "." 17 ] }
+
+    { Template =        "test {@test};{$test};test";   Values = [543;567]
+      ExpectedString =  "test 543;567;test"
+      Expected =        [ tx "test " 0
+                          dnp "test" 5; tx ";" 12
+                          snp "test" 13; tx ";test" 20 ] }
+
+    { Template =        "test {0};{1};{2}";   Values = [box "a"; box 1; box "z"]
+      ExpectedString =  "test a;1;z"
+      Expected =        [ tx "test " 0
+                          pp 0 5; tx ";" 8
+                          pp 1 9; tx ";" 12
+                          pp 2 13 ] }
+]
+
+let castToArray = Seq.cast<obj> >> Seq.toArray
+let formatEnumerable template enumerable =
+    format System.Globalization.CultureInfo.InvariantCulture
+           template
+           (castToArray enumerable)
+
+let runParseTest data =
+    let template = parse data.Template
+    printfn "TEST\n Expected = %s\n Actual   = %s" data.Template template.FormatString 
+    Xunit.Assert.Equal(data.Template, template.FormatString)
+//    printfn "Expected=%A" data.Expected
+//    printfn "Actual=%A" template.Tokens
+    Xunit.Assert.Equal<Token list>(data.Expected, template.Tokens)
+
+let runFormatTest data =
+    let template = parse data.Template
+    let actual = formatEnumerable template data.Values
+    Xunit.Assert.Equal(data.ExpectedString, actual)
+
+testData |> List.iter runParseTest
+testData |> List.iter runFormatTest
+
+
+
