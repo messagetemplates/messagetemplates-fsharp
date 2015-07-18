b14a3597adf63144d1d083cfa25575529705b3d3
Adam Chester <adamchester@gmail.com>
2015-07-18 20:47:37 +1000

    C# API, more F#, FsTest now do both langs

--- MessageTemplates.FsTests.net45/MessageTemplateTests.fs
+++ MessageTemplates.FsTests.net45/MessageTemplateTests.fs
@@ -35,155 +35,162 @@ let mttToToken (mtt: MessageTemplateToken) : Token =
 
 open Swensen.Unquote.Assertions
 open System.Globalization
+open System
 
-let parse (messageTemplate: string) =
-    // C# version
-    let csParser = MessageTemplates.Parsing.MessageTemplateParser()
-    let csTemplate = csParser.Parse(messageTemplate)
-    let csTokens = csTemplate.Tokens |> Seq.map mttToToken |> List.ofSeq
-    // F# version
-    let fsTemplate = FsMessageTemplates.MessageTemplates.parse messageTemplate
-    let fsTokens = fsTemplate.Tokens
-    // Ensure they're the same
-    test <@ csTokens = fsTokens @>
-    csTokens
+type LangTheoryAttribute() =
+    inherit Xunit.TheoryAttribute()
+type LangCsFsDataAttribute() =
+    inherit Xunit.Sdk.DataAttribute()
+    override __.GetData _ = [[|box "C#"|]; [|box "F#"|]] |> Seq.ofList
 
 let tokensToString tokens = 
-    let strings = tokens |> Seq.cast<Token> |> Seq.map (function | Text t -> t.ToString() | Prop (_, p) -> p.ToString()) |> Seq.toArray
+    let t2s = function | Text t -> t.ToString() | Prop (_, p) -> p.ToString()
+    let strings = tokens |> Seq.cast<Token> |> Seq.map t2s |> Seq.toArray
     System.String.Join("\n", strings)
 
-let assertParsedAs message (expectedTokens: System.Collections.IEnumerable) =
-    let parsed = parse message;
+let assertParsedAs lang message (expectedTokens: System.Collections.IEnumerable) =
+    let parsed =
+        match lang with
+        | "C#" -> MessageTemplates.Template.Parse(message).Tokens
+                  |> Seq.map mttToToken |> List.ofSeq
+        | "F#" -> (FsMessageTemplates.MessageTemplates.parse message).Tokens
+        | other -> failwithf "unexpected lang '%s'" other
+
     let expected = expectedTokens |> Seq.cast<Token> |> Seq.toList
-    test <@ expected = parsed  @>
+    test <@ parsed = expected @>
 
-[<Fact>]
-let ``an empty message is a single text token`` () = 
-    assertParsedAs "" [Tk.text 0 ""]
+[<LangTheory; LangCsFsData>]
+let ``an empty message is a single text token`` (lang) = 
+    assertParsedAs lang  "" [Tk.text 0 ""]
 
-[<Fact>]
-let ``a message without properties is a single text token`` () =
-    assertParsedAs "Hello, world!"
+[<LangTheory; LangCsFsData>]
+let ``a message without properties is a single text token`` (lang) =
+    assertParsedAs lang  "Hello, world!"
                    [Tk.text 0 "Hello, world!"]
 
-[<Fact>]
-let ``a message with property only is a single property token`` () =
+[<LangTheory; LangCsFsData>]
+let ``a message with property only is a single property token`` (lang) =
     let template = "{Hello}"
-    assertParsedAs template
+    assertParsedAs lang template
                    [Tk.prop 0 template "Hello"]
 
-[<Fact>]
-let ``doubled left brackets are parsed as a single bracket`` () =
+[<LangTheory; LangCsFsData>]
+let ``doubled left brackets are parsed as a single bracket`` (lang) =
     let template = "{{ Hi! }"
-    assertParsedAs template
+    assertParsedAs lang template
                    [Tk.text 0 "{ Hi! }"]
 
-[<Fact>]
-let ``doubled left brackets are parsed as a single bracket inside text`` () =
+[<LangTheory; LangCsFsData>]
+let ``doubled left brackets are parsed as a single bracket inside text`` (lang) =
     let template = "Well, {{ Hi!"
-    assertParsedAs template
+    assertParsedAs lang template
                    [Tk.text 0 "Well, { Hi!"]
 
-[<Fact>]
-let ``doubled right brackets are parsed as a single bracket`` () =
+[<LangTheory; LangCsFsData>]
+let ``doubled right brackets are parsed as a single bracket`` (lang) =
     let template = "Nice }}-: mo"
-    assertParsedAs template
+    assertParsedAs lang template
                    [Tk.text 0 "Nice }-: mo"]
 
-[<Fact>]
-let ``a malformed property tag is parsed as text`` () =
+[<LangTheory; LangCsFsData>]
+let ``a malformed property tag is parsed as text`` (lang) =
     let template = "{0 space}"
-    assertParsedAs template
+    assertParsedAs lang template
                    [Tk.text 0 template]
 
-[<Fact>]
-let ``an integer property name is parsed as positional property`` () =
+[<LangTheory; LangCsFsData>]
+let ``an integer property name is parsed as positional property`` (lang) =
     let template = "{0} text {1} other text {2}"
-    assertParsedAs template
+    assertParsedAs lang template
                    [Tk.propp 0 0
                     Tk.text 3 " text "
                     Tk.propp 9 1
                     Tk.text 12 " other text "
                     Tk.propp 24 2]
 
-[<Fact>]
-let ``formats can contain colons`` () =
+[<LangTheory; LangCsFsData>]
+let ``formats can contain colons`` (lang) =
     let template = "{Time:hh:mm}"
-    assertParsedAs template
+    assertParsedAs lang template
                    [Tk.propf 0 template "Time" "hh:mm" ]
 
-[<Fact>]
-let ``zero values alignment is parsed as text`` () =
+[<LangTheory; LangCsFsData>]
+let ``zero values alignment is parsed as text`` (lang) =
     let template1 = "{Hello,-0}"
-    assertParsedAs template1
+    assertParsedAs lang template1
                    [Tk.text 0 template1]
 
     let template2 = "{Hello,0}"
-    assertParsedAs template2
+    assertParsedAs lang template2
                    [Tk.text 0 template2]
  
  
-[<Fact>]
-let ``non-number alignment is parsed as text`` () =
+[<LangTheory; LangCsFsData>]
+let ``non-number alignment is parsed as text`` (lang) =
     let t1 = "{Hello,-aa}"
-    assertParsedAs t1 [Tk.text 0 t1]
+    assertParsedAs lang t1 [Tk.text 0 t1]
 
     let t2 = "{Hello,aa}"
-    assertParsedAs t2 [Tk.text 0 t2]
+    assertParsedAs lang t2 [Tk.text 0 t2]
 
     let t3 = "{Hello,-10-1}"
-    assertParsedAs t3 [Tk.text 0 t3]
+    assertParsedAs lang t3 [Tk.text 0 t3]
 
     let t4 = "{Hello,10-1}"
-    assertParsedAs t4 [Tk.text 0 t4]
+    assertParsedAs lang t4 [Tk.text 0 t4]
 
-[<Fact>]
-let ``empty alignment is parsed as text`` () =
+[<LangTheory; LangCsFsData>]
+let ``empty alignment is parsed as text`` (lang) =
     let t1 = "{Hello,}"
-    assertParsedAs t1 [Tk.text 0 t1]
+    assertParsedAs lang t1 [Tk.text 0 t1]
 
     let t2 = "{Hello,:format}"
-    assertParsedAs t2 [Tk.text 0 t2]
+    assertParsedAs lang t2 [Tk.text 0 t2]
 
-[<Fact>]
-let ``multiple tokens have the correct indexes`` () =
+[<LangTheory; LangCsFsData>]
+let ``multiple tokens have the correct indexes`` (lang) =
     let template = "{Greeting}, {Name}!"
-    assertParsedAs template
+    assertParsedAs lang template
                    [Tk.prop 0 "{Greeting}" "Greeting"
                     Tk.text 10 ", "
                     Tk.prop 12 "{Name}" "Name"
                     Tk.text 18 "!" ]
 
-[<Fact>]
-let ``missing right bracket is parsed as text`` () =
+[<LangTheory; LangCsFsData>]
+let ``missing right bracket is parsed as text`` (lang) =
     let template = "{Hello"
-    assertParsedAs template [Tk.text 0 template]
+    assertParsedAs lang template [Tk.text 0 template]
 
-[<Fact>]
-let ``destructure hint is parsed correctly`` () =
+[<LangTheory; LangCsFsData>]
+let ``destructure hint is parsed correctly`` (lang) =
     let template = "{@Hello}"
-    assertParsedAs template [Tk.propd 0 template "Hello"]
+    assertParsedAs lang template [Tk.propd 0 template "Hello"]
 
-[<Fact>]
-let ``stringify hint is parsed correctly`` () =
+[<LangTheory; LangCsFsData>]
+let ``stringify hint is parsed correctly`` (lang) =
     let template = "{$Hello}"
-    assertParsedAs template [Tk.propds 0 template "Hello"]
+    assertParsedAs lang template [Tk.propds 0 template "Hello"]
 
-[<Fact>]
-let ``destructuring with empty property name is parsed as text`` () =
+[<LangTheory; LangCsFsData>]
+let ``destructuring with empty property name is parsed as text`` (lang) =
     let template = "{@}"
-    assertParsedAs template [Tk.text 0 template]
+    assertParsedAs lang template [Tk.text 0 template]
     
-[<Fact>]
-let ``underscores are valid in property names`` () =
-    assertParsedAs "{_123_Hello}" [Tk.prop 0 "{_123_Hello}" "_123_Hello"]
+[<LangTheory; LangCsFsData>]
+let ``underscores are valid in property names`` (lang) =
+    assertParsedAs lang  "{_123_Hello}" [Tk.prop 0 "{_123_Hello}" "_123_Hello"]
 
-let inline renderp provider template args =
-    let template = FsMessageTemplates.MessageTemplates.parse template
-    format provider template (args |> Seq.cast<obj> |> Seq.toArray)
+let renderp lang (provider:IFormatProvider) messageTemplate args =
+    let argsArray = (args |> Seq.cast<obj> |> Seq.toArray) // force 'args' to be IEnumerable
+    match lang with
+    | "C#" -> MessageTemplates.Template.Format(provider, messageTemplate, argsArray)
+    | "F#" -> FsMessageTemplates.MessageTemplates.format provider
+                                                        (FsMessageTemplates.MessageTemplates.parse messageTemplate)
+                                                        argsArray
+    | other -> failwithf "unexpected lang '%s'" other
 
-let inline render template args =
-    renderp System.Globalization.CultureInfo.InvariantCulture template args
+let render lang template args =
+    renderp lang CultureInfo.InvariantCulture template args
 
 type Chair() =
     member __.Back with get() = "straight"
@@ -195,52 +202,47 @@ type Receipt() =
     member __.When with get() = System.DateTime(2013, 5, 20, 16, 39, 0)
     override __.ToString() = "a receipt"
 
-[<Fact>]
-let ``an object is rendered in simple notation`` () =
-    let m = render "I sat at {@Chair}" [Chair()]
+[<LangTheory; LangCsFsData>]
+let ``a class instance is rendered in simple notation`` (lang) =
+    let m = render lang "I sat at {@Chair}" [Chair()]
     test <@ m = "I sat at Chair { Back: \"straight\", Legs: [1, 2, 3, 4] }" @>
 
-[<Fact>]
-let ``an object is rendered in simple notation using format provider`` () =
-    let m = renderp (CultureInfo.GetCultureInfo("fr-FR")) "I received {@Receipt}" [Receipt()]
+[<LangTheory; LangCsFsData>]
+let ``a class instance is rendered in simple notation using format provider`` (lang) =
+    let m = renderp lang (CultureInfo.GetCultureInfo "fr-FR") "I received {@Receipt}" [Receipt()]
     test <@ m = "I received Receipt { Sum: 12,345, When: 20/05/2013 16:39:00 }" @>
 
 type ChairRecord = { Back:string; Legs: int array }
 
-[<Fact>]
-let ``an anonymous object is rendered in simple notation without type`` () =
-    let m = render "I sat at {@Chair}" [{ Back="straight"; Legs=[|1;2;3;4|] }]
-    test <@ m = "I sat at { Back: \"straight\", Legs: [1, 2, 3, 4] }" @>
+[<LangTheory; LangCsFsData>]
+let ``an F# record object is rendered in simple notation with type`` (lang) =
+    let m = render lang "I sat at {@Chair}" [{ Back="straight"; Legs=[|1;2;3;4|] }]
+    test <@ m = "I sat at ChairRecord { Back: \"straight\", Legs: [1, 2, 3, 4] }" @>
+
+type ReceiptRecord = { Sum: double; When: System.DateTime }
+
+[<LangTheory; LangCsFsData>]
+let ``an F# record object is rendered in simple notation with type using format provider`` (lang) =
+    let m = renderp lang (CultureInfo.GetCultureInfo "fr-FR")
+                    "I received {@Receipt}" [{ Sum=12.345; When=DateTime(2013, 5, 20, 16, 39, 0) }]
+    test <@ m = "I received ReceiptRecord { Sum: 12,345, When: 20/05/2013 16:39:00 }" @>
+
+[<LangTheory; LangCsFsData>]
+let ``an object with default destructuring is rendered as a string literal`` (lang) =
+    let m = render lang "I sat at {Chair}" [Chair()]
+    test <@ m = "I sat at \"a chair\"" @>
+
+[<LangTheory; LangCsFsData>]
+let ``an object with stringify destructuring is rendered as a string`` (lang) =
+    let m = render lang "I sat at {$Chair}" [Chair()]
+    test <@ m = "I sat at \"a chair\"" @>
+
+[<LangTheory; LangCsFsData>]
+let ``multiple properties are rendered in order`` (lang) =
+    let m = render lang "Just biting {Fruit} number {Count}" [box "Apple"; box 12]
+    test <@ m = "Just biting \"Apple\" number 12" @>
 
 (** TODO
-
-[Fact]
-public void AnAnonymousObjectIsRenderedInSimpleNotationWithoutTypeUsingFormatProvider()
-{
-    var m = Render(CultureInfo.GetCultureInfo("fr-FR"), "I received {@Receipt}", new { Sum = 12.345, When = new DateTime(2013, 5, 20, 16, 39, 0) });
-    Assert.Equal("I received { Sum: 12,345, When: 20/05/2013 16:39:00 }", m);
-}
-
-[Fact]
-public void AnObjectWithDefaultDestructuringIsRenderedAsAStringLiteral()
-{
-    var m = Render("I sat at {Chair}", new Chair());
-    Assert.Equal("I sat at \"a chair\"", m);
-}
-
-[Fact]
-public void AnObjectWithStringifyDestructuringIsRenderedAsAString()
-{
-    var m = Render("I sat at {$Chair}", new Chair());
-    Assert.Equal("I sat at \"a chair\"", m);
-}
-
-[Fact]
-public void MultiplePropertiesAreRenderedInOrder()
-{
-    var m = Render("Just biting {Fruit} number {Count}", "Apple", 12);
-    Assert.Equal("Just biting \"Apple\" number 12", m);
-}
 
 [Fact]
 public void MultiplePropertiesUseFormatProvider()
---
+++ MessageTemplates/MessageTemplate.cs
@@ -0,0 +1,59 @@
+﻿using MessageTemplates.Events;
+using MessageTemplates.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MessageTemplates
+{
+    /// <summary>
+    /// The main user-facing API with methods for parsing, formatting, and
+    /// capturing properties from message templates.
+    /// </summary>
+    public static class Template
+    {
+        public static MessageTemplate Parse(string templateMessage)
+        {
+            return new MessageTemplateParser().Parse(templateMessage);
+        }
+
+        public static IEnumerable<LogEventProperty> Capture(MessageTemplate template, params object[] args)
+        {
+            var binder = new MessageTemplates.Parameters.PropertyBinder(
+                new MessageTemplates.Parameters.PropertyValueConverter(
+                    10, Enumerable.Empty<Type>(), Enumerable.Empty<MessageTemplates.Core.IDestructuringPolicy>()));
+
+            return binder.ConstructProperties(template, args);
+        }
+
+        public static void Format(
+            IFormatProvider formatProvider,
+            System.IO.TextWriter output,
+            string templateMessage,
+            params object[] args)
+        {
+            var template = Parse(templateMessage);
+            var props = Capture(template, args);
+            template.Render(props.ToDictionary40(x => x.Name, x => x.Value), output, formatProvider);
+        }
+
+        public static string Format(
+            IFormatProvider formatProvider,
+            string templateMessage,
+            params object[] args)
+        {
+            var sw = new System.IO.StringWriter(formatProvider);
+            Format(formatProvider, sw, templateMessage, args);
+            sw.Flush();
+            return sw.ToString();
+        }
+
+        public static string Format(
+            string templateMessage,
+            params object[] args)
+        {
+            return Format(System.Globalization.CultureInfo.InvariantCulture, templateMessage, args);
+        }
+    }
+}
