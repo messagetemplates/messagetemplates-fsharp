253a84ae2531f60d105dca8e2352f2c75fa8234a
Adam Chester <adamchester@gmail.com>
2015-07-24 14:59:34 +1000

    C#: cleanup public xml comment

--- MessageTemplates/MessageTemplate.cs
+++ MessageTemplates/MessageTemplate.cs
@@ -2,8 +2,9 @@
 using MessageTemplates.Parsing;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
-using System.Text;
 
 namespace MessageTemplates
 {
@@ -13,47 +14,71 @@ namespace MessageTemplates
     /// </summary>
     public static class Template
     {
+        /// <summary>
+        /// Parses a message template (e.g. "hello, {name}") into a 
+        /// <see cref="MessageTemplate"/> structure.
+        /// </summary>
+        /// <param name="templateMessage">A message template (e.g. "hello, {name}")</param>
+        /// <returns>The parsed message template.</returns>
         public static MessageTemplate Parse(string templateMessage)
         {
             return new MessageTemplateParser().Parse(templateMessage);
         }
 
-        public static IEnumerable<LogEventProperty> Capture(MessageTemplate template, params object[] args)
+        /// <summary>
+        /// Captures properties from the given message template and 
+        /// provided values.
+        /// </summary>
+        public static IEnumerable<LogEventProperty> Capture(
+            MessageTemplate template, params object[] values)
         {
-            var binder = new MessageTemplates.Parameters.PropertyBinder(
-                new MessageTemplates.Parameters.PropertyValueConverter(
-                    10, Enumerable.Empty<Type>(), Enumerable.Empty<MessageTemplates.Core.IDestructuringPolicy>()));
+            var binder = new Parameters.PropertyBinder(
+                new Parameters.PropertyValueConverter(
+                    10,
+                    Enumerable.Empty<Type>(),
+                    Enumerable.Empty<Core.IDestructuringPolicy>()));
 
-            return binder.ConstructProperties(template, args);
+            return binder.ConstructProperties(template, values);
         }
 
+        /// <summary>
+        /// Formats the message template as a string, written into the text
+        /// writer.
+        /// </summary>
         public static void Format(
             IFormatProvider formatProvider,
-            System.IO.TextWriter output,
+            TextWriter output,
             string templateMessage,
-            params object[] args)
+            params object[] values)
         {
             var template = Parse(templateMessage);
-            var props = Capture(template, args);
+            var props = Capture(template, values);
             template.Render(props.ToDictionary40(x => x.Name, x => x.Value), output, formatProvider);
         }
 
+        /// <summary>
+        /// Formats the message template as a string using the provided
+        /// format provider and values.
+        /// </summary>
         public static string Format(
             IFormatProvider formatProvider,
             string templateMessage,
-            params object[] args)
+            params object[] values)
         {
-            var sw = new System.IO.StringWriter(formatProvider);
-            Format(formatProvider, sw, templateMessage, args);
+            var sw = new StringWriter(formatProvider);
+            Format(formatProvider, sw, templateMessage, values);
             sw.Flush();
             return sw.ToString();
         }
 
+        /// <summary>
+        /// Formats the message template as a string using the provided values.
+        /// </summary>
         public static string Format(
             string templateMessage,
-            params object[] args)
+            params object[] values)
         {
-            return Format(System.Globalization.CultureInfo.InvariantCulture, templateMessage, args);
+            return Format(CultureInfo.InvariantCulture, templateMessage, values);
         }
     }
 }
