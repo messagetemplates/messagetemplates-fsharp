7de96b8b6c639aa95461e205fb20e9a877ff7dbe
Adam Chester <adamchester@gmail.com>
2015-08-01 21:40:44 +1000

    Paket auto-restore and appveyor

--- test.cmd
+++ test.cmd
@@ -1,3 +1,3 @@
 dnx MessageTemplates.Tests test
 packages\xunit.runner.console\tools\xunit.console.x86.exe FsMessageTemplates.Tests\bin\Release\FsMessageTemplates.Tests.dll
-packages\xunit.runner.console\tools\xunit.console.x86.exe FsMessageTemplates.Tests\bin\Release\FsMessageTemplates.Tests.dll
+mstest /testcontainer:MessageTemplates.PerfTests\bin\Release\MessageTemplates.PerfTests.dll
