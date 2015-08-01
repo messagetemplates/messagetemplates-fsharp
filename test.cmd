6bcad032c7f807295f24e4b6c1d53bbbc44e8522
Adam Chester <adamchester@gmail.com>
2015-08-01 22:56:12 +1000

    fixup vstest for appveyor & local

--- test.cmd
+++ test.cmd
@@ -1,3 +1,8 @@
 dnx MessageTemplates.Tests test
-packages\xunit.runner.console\tools\xunit.console.x86.exe FsMessageTemplates.Tests\bin\Release\FsMessageTemplates.Tests.dll
-vstest.console /logger:AppVeyor /Logger:timelineLogger MessageTemplates.PerfTests\bin\Release\MessageTemplates.PerfTests.dll
+packages\xunit.runner.console.2.0.0\tools\xunit.console.x86.exe FsMessageTemplates.Tests\bin\Release\FsMessageTemplates.Tests.dll
+
+IF ['%APPVEYOR%'] == ['True'] (
+	vstest.console /Logger:AppVeyor MessageTemplates.PerfTests\bin\Release\MessageTemplates.PerfTests.dll
+) ELSE (
+	vstest.console /Logger:timelineLogger MessageTemplates.PerfTests\bin\Release\MessageTemplates.PerfTests.dll
+)
