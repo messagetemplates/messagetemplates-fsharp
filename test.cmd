3d0a7b76066f8be657e6571ff90f54a6ccc63d41
Adam Chester <adamchester@gmail.com>
2015-08-01 23:24:29 +1000

    Update test.cmd

--- test.cmd
+++ test.cmd
@@ -1,5 +1,5 @@
 dnx MessageTemplates.Tests test
-packages\xunit.runner.console.2.0.0\tools\xunit.console.x86.exe FsMessageTemplates.Tests\bin\Release\FsMessageTemplates.Tests.dll
+nunit-console-x86 FsMessageTemplates.Tests\bin\Release\FsMessageTemplates.Tests.dll
 
 IF ['%APPVEYOR%'] == ['True'] (
 	vstest.console /Logger:AppVeyor MessageTemplates.PerfTests\bin\Release\MessageTemplates.PerfTests.dll
