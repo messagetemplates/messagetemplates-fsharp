f1ca53752033da92cbbf3bbc49f872f4a5a9361a
Adam Chester <adamchester@gmail.com>
2015-08-01 21:05:34 +1000

    Switch to paket

---
+++ test.cmd
@@ -0,0 +1,3 @@
+dnx MessageTemplates.Tests test
+packages\xunit.runner.console\tools\xunit.console.x86.exe FsMessageTemplates.Tests\bin\Release\FsMessageTemplates.Tests.dll
+packages\xunit.runner.console\tools\xunit.console.x86.exe FsMessageTemplates.Tests\bin\Release\FsMessageTemplates.Tests.dll
