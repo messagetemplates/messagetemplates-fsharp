7ee03de0c14884fd158fa104a00aafb2666a5a2c
Adam Chester <adamchester@gmail.com>
2015-08-14 18:29:01 +1000

    fixed appveyor vstest logger

--- test.cmd
+++ test.cmd
@@ -3,7 +3,7 @@ dnx MessageTemplates.Tests test
 packages\xunit.runner.console\tools\xunit.console.x86.exe FsMessageTemplates.Tests\bin\Release\FsMessageTemplates.Tests.dll
 
 IF ['%APPVEYOR%'] == ['True'] (
-	vstest.console /Logger:AppVeyor /Logger:timelineLogger MessageTemplates.PerfTests\bin\Release\MessageTemplates.PerfTests.exe
+	vstest.console /Logger:AppVeyor MessageTemplates.PerfTests\bin\Release\MessageTemplates.PerfTests.exe
 ) ELSE (
 	vstest.console /Logger:timelineLogger MessageTemplates.PerfTests\bin\Release\MessageTemplates.PerfTests.exe
 )
