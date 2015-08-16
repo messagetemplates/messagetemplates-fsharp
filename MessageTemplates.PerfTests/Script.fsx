dae9270ac37401539252acfd3aaddf2be288000f
Adam Chester <adamchester@gmail.com>
2015-08-16 21:17:42 +1000

    generate perf images

--- MessageTemplates.PerfTests/Script.fsx
+++ MessageTemplates.PerfTests/Script.fsx
@@ -2,9 +2,6 @@
 #I "../packages/PerfUtil/lib/net40"
 #r "PerfUtil"
 
-#I "../packages/suave/lib/net40"
-#r "Suave"
-
 #r "System.IO"
 #I "bin/Release/"
 #r "FsMessageTemplates"
@@ -15,7 +12,7 @@ open PerfUtil
 open FsMessageTemplates.MessageTemplates
 open MessageTemplates
 
-module ParseFormat =
+module StringFormatComptible =
     type IParseFormatTest =
         inherit ITestable
         abstract ParseFormat : string -> obj[] -> unit
@@ -35,32 +32,143 @@ module ParseFormat =
     let MtCs = parseFormatTest "MessageT C#" (fun tw template args ->
         MessageTemplate.Format(tw.FormatProvider, tw, template, args))
 
-    let TwCs = parseFormatTest "TextWriter C#" (fun tw template args ->
+    let TwCs = parseFormatTest "TextWriter" (fun tw template args ->
         tw.Write(template, args))
 
-let mtCsFsAndTextWriterTests = new ImplementationComparer<ParseFormat.IParseFormatTest>(
-                                ParseFormat.MtFs, [ ParseFormat.MtCs; ParseFormat.TwCs ])
+    let positional = new ImplementationComparer<IParseFormatTest>(
+                                        TwCs, [ MtFs; MtCs; ],
+                                        warmup=true, verbose=true)
 
-mtCsFsAndTextWriterTests.Run(id="10k x 3 pos args, all ints", repeat=10000,
-    testF=(fun o -> o.ParseFormat ("{0} {1} {2}") [|0;1;2|]))
+    let namedAndDestruring = new ImplementationComparer<IParseFormatTest>(
+                                    MtFs, [ MtCs ], warmup=true, verbose=true)
 
-mtCsFsAndTextWriterTests.Run(id="10k x 5 pos args, all strings", repeat=10000,
-    testF=(fun o -> o.ParseFormat ("{0}{2}{1}{4}") [|0;1;2;3;4|]))
+StringFormatComptible.positional.Run(
+    id="10k x 3 posit. ints (AR5)", repeat=10000,
+    testF=(fun o -> o.ParseFormat ("big long x{0,5:0,00}x{1,5:0,00}x{2,5:0,00}x") [|0;1;2|]))
 
+StringFormatComptible.positional.Run(
+    id="10k x 3 posit. ints fmt", repeat=10000,
+    testF=(fun o -> o.ParseFormat ("big long x{0:0,00}x{1:0,00}x{2:0,00}x") [|0;1;2|]))
 
-let namedTests = new ImplementationComparer<ParseFormat.IParseFormatTest>(
-                                ParseFormat.MtFs, [ ParseFormat.MtCs ])
+StringFormatComptible.positional.Run(
+    id="10k x 5 posit. strings", repeat=10000,
+    testF=(fun o -> o.ParseFormat ("big long {0}x{2}x{1}x{4}x{3}") [|"a";"b";"c";"d";"e"|]))
 
 System.GC.Collect()
-namedTests.Run(id="100k x named destr", repeat=100000,
-    testF=(fun o -> o.ParseFormat "{@one} {@two} {@three}"
+StringFormatComptible.namedAndDestruring.Run(
+    id="100k x 3 named destr", repeat=100000,
+    testF=(fun o -> o.ParseFormat "big long {@one} {@two} {@three}"
                                   [| System.Version(1, 1)
                                      (222, 222)
                                      System.ConsoleColor.Magenta |]))
 
 System.GC.Collect()
-namedTests.Run(id="100k x named destr (AL10)", repeat=100000,
+StringFormatComptible.namedAndDestruring.Run(
+    id="100k x 3 named destr (AL10)", repeat=100000,
     testF=(fun o -> o.ParseFormat "{@one,-10} {@two,-10} {@three,-10}"
                                   [| System.Version(1, 1)
                                      (222, 222)
                                      System.ConsoleColor.Magenta |]))
+
+type IFormatTest =
+    inherit ITestable
+    abstract DoIt : unit -> unit
+    
+let anyFormatTest name run =
+    { new IFormatTest with
+        member __.Name = name
+        member __.DoIt () = run ()
+        member __.Fini () = ()
+        member __.Init () = () }
+
+let invariant = System.Globalization.CultureInfo.InvariantCulture
+
+module AnyFormat =
+    let args = [| box "adam" |]
+    let MtFs = anyFormatTest "MessageT F#" (fun () ->
+        sprintsm invariant "Hello, {name}" args |> ignore )
+
+    let MtCs = anyFormatTest "MessageT C#" (fun () ->
+        MessageTemplate.Format(invariant, "Hello, {name}", args) |> ignore)
+
+    let StringFormat = anyFormatTest "String.Format" (fun () ->
+        System.String.Format(invariant, "Hello, {0}", args) |> ignore)
+
+    let StringWriter = anyFormatTest "StringWriter" (fun () ->
+        (new System.IO.StringWriter(invariant)).Write("Hello, {0}", args))
+
+    let StringBuilder = anyFormatTest "StringBuilder" (fun () ->
+        System.Text.StringBuilder().AppendFormat(invariant, "Hello, {0}", args) |> ignore)
+
+    let FsPrintf = anyFormatTest "F# sprintf" (fun () ->
+        Printf.sprintf "Hello, %s" "adam" |> ignore)
+
+    let allBasicFormatters = new ImplementationComparer<IFormatTest>(
+                                    StringBuilder, [ MtFs; MtCs; StringFormat
+                                                     FsPrintf; StringWriter],
+                                    warmup=true, verbose=true)
+
+AnyFormat.allBasicFormatters.Run(
+    id="Format [|\"adam\"|]", repeat=100000,
+    testF=fun t -> t.DoIt())
+
+module FormatStringify =
+    let theVersion = System.Version(2, 2, 2, 2)
+    let args = [| box theVersion |]
+
+    let MtFs = anyFormatTest "MessageT F#" (fun () ->
+        sprintsm invariant "Release {$version}" args |> ignore )
+
+    let MtCs = anyFormatTest "MessageT C#" (fun () ->
+        MessageTemplate.Format(invariant, "Release {$version}", args) |> ignore)
+
+    let StringFormat = anyFormatTest "String.Format" (fun () ->
+        System.String.Format(invariant, "Release {0}", args) |> ignore)
+
+    let StringWriter = anyFormatTest "StringWriter" (fun () ->
+        (new System.IO.StringWriter(invariant)).Write("Release {0}", args))
+
+    let StringBuilder = anyFormatTest "StringBuilder" (fun () ->
+        System.Text.StringBuilder().AppendFormat(invariant, "Release {0}", args) |> ignore)
+
+    let FsPrintf = anyFormatTest "F# sprintf" (fun () ->
+        Printf.sprintf "Release %O" theVersion |> ignore)
+
+    let allBasicFormatters = new ImplementationComparer<IFormatTest>(
+                                    StringBuilder, [ MtCs; MtFs
+                                                     StringFormat; StringWriter
+                                                     FsPrintf ],
+                                    warmup=true, verbose=true)
+
+FormatStringify.allBasicFormatters.Run(
+    id="Format stringify Version", repeat=100000,
+    testF=fun t -> t.DoIt())
+
+#load "../packages/FSharp.Charting/FSharp.Charting.fsx"
+
+open FSharp.Charting
+open PerfUtil
+
+// simple plot function
+let plot yaxis (metric : PerfResult -> float) (results : PerfResult list) =
+    let values = results |> List.choose (fun r -> if r.HasFailed then None else Some (r.SessionId, metric r))
+    let name = results |> List.tryPick (fun r -> Some r.TestId)
+    let ch = Chart.Bar(values, ?Name = name, ?Title = name, YTitle = yaxis)
+    let nameFixedForExport = name.Value.ToString().Replace("[|\"","_").Replace("\"|]", "_") + ".png"
+    let exportFolder = System.IO.Path.Combine(__SOURCE_DIRECTORY__, "..\\artifacts\\perfcharts\\")
+    System.IO.Directory.CreateDirectory(exportFolder) |> ignore
+    let fileName = System.IO.Path.Combine(exportFolder, nameFixedForExport)
+    System.Console.WriteLine("saving {0}", fileName)
+    ch.ShowChart() |> ignore
+    ch.SaveChartAs (fileName, ChartTypes.ChartImageFormat.Png)
+
+// read performance tests from 'Tests' module and run them
+let results =
+    [ AnyFormat.allBasicFormatters.GetTestResults()
+      FormatStringify.allBasicFormatters.GetTestResults()
+      StringFormatComptible.namedAndDestruring.GetTestResults()
+      StringFormatComptible.positional.GetTestResults()
+    ]
+    |> List.concat
+    |> TestSession.groupByTest
+    |> Map.iter (fun _ r -> plot "milliseconds" (fun t -> t.Elapsed.TotalMilliseconds) r)
