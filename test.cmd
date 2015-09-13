17434a0f357a7e2fdafc75f979824b8dac76e0c9
Adam Chester <adamchester@gmail.com>
2015-09-13 15:25:01 +1000

    back to dnx beta6 for build success

--- test.cmd
+++ test.cmd
@@ -1,4 +1,5 @@
-dnx -p MessageTemplates.Tests test
+REM dnx -p MessageTemplates.Tests test
+dnx MessageTemplates.Tests test
 @if ERRORLEVEL 1 goto :failure
 
 packages\xunit.runner.console\tools\xunit.console.x86.exe FsMessageTemplates.Tests\bin\Release\FsMessageTemplates.Tests.dll
@@ -16,10 +17,10 @@ IF ['%APPVEYOR%'] == ['True'] (
 	"set PATH=%PATH%;C:\\Program Files (x86)\\Microsoft SDKs\\F#\\3.1\\Framework\\v4.0\\""
 ) 
 
-FsiAnyCPU -O --tailcalls+ --crossoptimize+ --exec MessageTemplates.PerfTests\Script.fsx
+fsi -O --tailcalls+ --crossoptimize+ --exec MessageTemplates.PerfTests\Script.fsx
 @if ERRORLEVEL 1 goto :failure
 
-FsiAnyCPU -O --tailcalls+ --crossoptimize+ --exec MessageTemplates.PerfTests\PerfTests.fsx
+fsi -O --tailcalls+ --crossoptimize+ --exec MessageTemplates.PerfTests\PerfTests.fsx
 @if ERRORLEVEL 1 goto :failure
 
 goto :eof
