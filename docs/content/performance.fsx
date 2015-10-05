75670507263af072da8fdb3caf31d1c6b2bce089
Adam Chester <adamchester@gmail.com>
2015-10-05 17:27:03 +1000

    perf page

--- docs/content/performance.fsx
+++ docs/content/performance.fsx
@@ -8,5 +8,16 @@
 Performance
 ===========
 
+![1](img/perfcharts/10k x 3 named destr (AL10).png)
+![2](img/perfcharts/10k x 3 named destr.png)
+![3](img/perfcharts/10k x 3 posit. ints (AR5).png)
+![4](img/perfcharts/10k x 3 posit. ints fmt.png)
+![5](img/perfcharts/10k x 5 posit. strings.png)
+![6](img/perfcharts/10k x format cached template.png)
+![7](img/perfcharts/10k x Format stringify Version.png)
+![8](img/perfcharts/10k x named 1x string.png)
+![9](img/perfcharts/10k x named 2x string.png)
+![10](img/perfcharts//10k x same pos 7 destr.png)
+
 *)
 
