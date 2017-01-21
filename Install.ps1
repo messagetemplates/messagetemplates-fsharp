59dbb15607abbe3f67aee1e2561385730e2f65f9
Adam Chester <adamchester@gmail.com>
2017-01-21 13:44:13 +1000

    1.0.0-rc4-004590

--- Install.ps1
+++ Install.ps1
@@ -1,6 +1,6 @@
 mkdir -Force ".\build\" | Out-Null
 Invoke-WebRequest "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.ps1" -OutFile ".\build\installcli.ps1"
 $env:DOTNET_INSTALL_DIR = "$pwd\.dotnetcli"
-& .\build\installcli.ps1 -InstallDir "$env:DOTNET_INSTALL_DIR" -NoPath -Version 1.0.0-rc4-004553
+& .\build\installcli.ps1 -InstallDir "$env:DOTNET_INSTALL_DIR" -NoPath -Version 1.0.0-rc4-004590
 $env:Path = "$env:DOTNET_INSTALL_DIR;$env:Path"
 
