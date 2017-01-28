35953fc807467ee6cd71b6b46d2db0c248effea5
Adam Chester <adamchester@gmail.com>
2017-01-28 10:21:05 +1000

    Travis/dotnet-cli on OS X

---
+++ tooling/configure-dotnet-cli-osx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+brew config
+
+# instructions taken from https://www.microsoft.com/net/core#macos
+brew update
+brew install openssl
+mkdir -p /usr/local/lib
+ln -s /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib /usr/local/lib/
+ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib/
