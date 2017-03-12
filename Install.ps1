e8c0f76dbd32cd1d0f8e447983869c23e5e287ee
Adam Chester <adamchester@gmail.com>
2017-03-13 07:19:06 +1000

    dotnet CLI tools v1.0.1

--- Install.ps1
+++ Install.ps1
@@ -1,6 +1,6 @@
 mkdir -Force ".\build\" | Out-Null
 Invoke-WebRequest "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.ps1" -OutFile ".\build\installcli.ps1"
 $env:DOTNET_INSTALL_DIR = "$pwd\.dotnetcli"
-& .\build\installcli.ps1 -InstallDir "$env:DOTNET_INSTALL_DIR" -NoPath -Version 1.0.0
+& .\build\installcli.ps1 -InstallDir "$env:DOTNET_INSTALL_DIR" -NoPath -Version 1.0.1
 $env:Path = "$env:DOTNET_INSTALL_DIR;$env:Path"
 
