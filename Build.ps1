45fbd235733e0a908bbf1bab9d34055b0e4220ae
Adam Chester <adamchester@gmail.com>
2017-11-12 10:54:51 +1000

    updated build ps1

--- Build.ps1
+++ Build.ps1
@@ -3,26 +3,34 @@ echo "build: Build started"
 Push-Location $PSScriptRoot
 
 if(Test-Path .\artifacts) {
-        echo "build: Cleaning .\artifacts"
-        Remove-Item .\artifacts -Force -Recurse
+	echo "build: Cleaning .\artifacts"
+	Remove-Item .\artifacts -Force -Recurse
 }
 
 & dotnet --version
-& dotnet restore
+& dotnet restore --no-cache
 
 $branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
-$branch = $branch.Substring(0, [math]::Min(8, $branch.Length))
-$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "l" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "l"]
+$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
+$commitHash = $(git rev-parse --short HEAD)
+$buildSuffix = @{ $true = "$($suffix)-$($commitHash)"; $false = "$($branch)-$($commitHash)" }[$suffix -ne ""]
 
-echo "build: Version suffix is $suffix"
+echo "build: Package version suffix is $suffix"
+echo "build: Build version suffix is $buildSuffix"
 
 foreach ($src in ls src/*) {
     Push-Location $src
 
-    echo "build: Packaging project in $src"
+	echo "build: Packaging project in $src"
 
-    & dotnet pack --include-symbols -c Release -o ..\..\artifacts --version-suffix=$suffix
+    & dotnet build -c Release --version-suffix=$buildSuffix
+
+    if($suffix) {
+        & dotnet pack -c Release --include-source --no-build -o ..\..\artifacts --version-suffix=$suffix
+    } else {
+        & dotnet pack -c Release --include-source --no-build -o ..\..\artifacts
+    }
     if($LASTEXITCODE -ne 0) { exit 1 }
 
     Pop-Location
@@ -31,7 +39,7 @@ foreach ($src in ls src/*) {
 foreach ($test in ls test/*.Tests) {
     Push-Location $test
 
-    echo "build: Testing project in $test"
+	echo "build: Testing project in $test"
 
     & dotnet test -c Release
     if($LASTEXITCODE -ne 0) { exit 3 }
